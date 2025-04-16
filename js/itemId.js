49a56c05d113d9aec41171df9e0dba45dad74142
Mathis Burger <handymathis@gmail.com>
2025-04-16 13:44:17 +0200

    feat: Added scripts of quantity containers to an extra file

--- js/itemId.js
+++ js/itemId.js
@@ -1,10 +1,3 @@
-const containers = document.getElementsByClassName("quantity-container");
-for (const container of containers) {
-    const input = container.children.item(1);
-    container.children.item(0).addEventListener('click', () => input.value = parseInt(`${input.value}`, 10)-1);
-    container.children.item(2).addEventListener('click', () => input.value = parseInt(`${input.value}`, 10)+1);
-}
-
 /**
  * Handles the form submission on the item ID page
  *
