4a1c366365f63da10b20b3cea171012408a8d4d4
Mathis Burger <handymathis@gmail.com>
2025-04-30 14:38:20 +0200

    feat: Hey hey sidebar hey sidebar hey

--- public/js/header.js
+++ public/js/header.js
@@ -1,13 +1,16 @@
 const sidebarToggler = document.getElementById("sidebarDrawerToggler");
 const sidebarDrawer = document.getElementById("sidebarDrawer");
+const sidebarOverlay = document.getElementById("sidebarOverlay");
 const sidebarClose = document.getElementById("sidebarClose")
 
 sidebarToggler.addEventListener("click", () => {
     sidebarDrawer.classList.toggle("open");
+    sidebarOverlay.classList.toggle("open");
 });
 
 sidebarClose.addEventListener("click", () => {
     sidebarDrawer.classList.toggle("open");
+    sidebarOverlay.classList.toggle("open");
 });
 
 document.addEventListener("click", (e) => {
@@ -18,5 +21,6 @@ document.addEventListener("click", (e) => {
        && !(sidebarToggler.contains(e.target) || sidebarToggler === e.target)
    ) {
        sidebarDrawer.classList.remove("open");
+       sidebarOverlay.classList.remove("open");
    }
 });
