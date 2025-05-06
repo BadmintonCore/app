f1cb02930956f3975ef4206b2d4bb2b3ff0c4651
Mathis Burger <handymathis@gmail.com>
2025-05-06 18:33:34 +0200

    feat: Completed initial PHP file serving approach with centralization

---
+++ database/Repositories/CategoryRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Vestis\Database\Repositories;
+
+use Vestis\Database\Models\Category;
+
+class CategoryRepository
+{
+
+    /**
+     * Gets all categories that do not have a parent category
+     *
+     * @return array<int, Category>
+     */
+    public static function findAllWithNoParent(): array
+    {
+        return QueryAbstraction::fetchManyAs(Category::class, "SELECT * FROM category WHERE parentCategoryId IS NULL");
+    }
+
+}
