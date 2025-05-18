3f5b23c23d3103ccba125d4d1f17ad50ef77cabd
lassehoffmann <hoffmann.lasse@t-online.de>
2025-05-18 19:47:37 +0200

    changed reset view

--- views/auth/reset.php
+++ views/auth/reset.php
@@ -2,13 +2,13 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <?php include(__DIR__."/../../components/head.php"); ?>
+    <?php include(__DIR__ . "/../../components/head.php"); ?>
     <title>vestis. - Passwort zurücksetzen</title>
 </head>
 <body>
 
 <!--Header der Website-->
-<?php include(__DIR__."/../../components/header.php"); ?>
+<?php include(__DIR__ . "/../../components/header.php"); ?>
 
 <!--Inhalt der Seite-->
 <main>
@@ -22,15 +22,17 @@
         </h1>
 
         <!--Zurückbutton-->
-        <?php include(__DIR__."/../../components/back-btn.php"); ?>
+        <?php include(__DIR__ . "/../../components/back-btn.php"); ?>
 
         <?php if (isset($errorMessage)) : ?>
             <h4 class="error-message"><?= $errorMessage ?></h4>
         <?php endif; ?>
         <?php if (isset($successMessage)) : ?>
             <h4 class="success-message"><?= $successMessage ?></h4>
-        <?php endif; ?>
-
+            <a href="/auth/login" class="btn">
+                login.
+            </a>
+        <?php else: ?>
         <!--Container der Klasse "form-input"-->
         <div class="form-input">
             <label for="mail">
@@ -46,11 +48,12 @@
             zurücksetzen.
         </button>
     </form>
+    <?php endif; ?>
 </main>
 
 <!--Footer der Website-->
-<?php include(__DIR__."/../../components/footer.php"); ?>
-<?php include(__DIR__."/../../components/scripts.php"); ?>
+<?php include(__DIR__ . "/../../components/footer.php"); ?>
+<?php include(__DIR__ . "/../../components/scripts.php"); ?>
 </body>
 </html>
 <!--Author: Lasse Hoffmann-->
