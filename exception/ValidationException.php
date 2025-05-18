978c26f49dd22d0a5cecfe4acd70d1f6e89ce64c
Mathis Burger <handymathis@gmail.com>
2025-05-18 20:55:27 +0200

    fix: Removed default import namespace

--- exception/ValidationException.php
+++ exception/ValidationException.php
@@ -2,14 +2,13 @@
 
 namespace Vestis\Exception;
 
-use Throwable;
 
 /**
  * Exception that is thrown if any type of validation error occurs.
  */
 class ValidationException extends \Exception
 {
-    public function __construct(string $message, int $code = 0, ?Throwable $previous = null)
+    public function __construct(string $message, int $code = 0, ?\Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
